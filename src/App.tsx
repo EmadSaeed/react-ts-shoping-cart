cf11d7c5f6960feffe9f077fee5b259a5f8bfcdf
Emad Saeed <hello@emadsaeed.com>
2020-08-10 18:47:24 +0100

    routers created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-
+import {Routes, Route} from 'react-router-dom';
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Store from "./components/pages/Store";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/'><Home /></Route>
+        <Route path='/about'><About /></Route>
+        <Route path='/store'><Store /></Route>
+      </Routes>
     </div>
   );
 }
